838a028c488d400d674f901e1636ddef68ab14b3
Emily Ramirez <123614468+EMMRAMIREZ@users.noreply.github.com>
2026-04-30 09:07:59 -0400

    Create int_database.sql

---
+++ scripts/int_database.sql
@@ -0,0 +1,26 @@
+/* Create Databases 
+
+Script Purpose:
+	This script creates a new Database named 'DataWarehouse1'and creates three other databases for each of the layers 
+    -'bronze', 'silver', and 'gold'.
+
+
+*/
+
+
+
+CREATE DATABASE DataWarehouse;
+
+CREATE DATABASE bronze;
+
+CREATE DATABASE silver;
+
+CREATE DATABASE gold;
+
+SHOW DATABASES;
+
+
+
+
+
+
