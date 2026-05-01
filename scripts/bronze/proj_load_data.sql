b4abbefc9adf681861a2b8002071cd1f3c91f295
Emily Ramirez <123614468+EMMRAMIREZ@users.noreply.github.com>
2026-04-30 20:32:40 -0400

    Create proj_load_data.sql

---
+++ scripts/bronze/proj_load_data.sql
@@ -0,0 +1,61 @@
+-- Loads data into all 6 tables, code will check that any exsisting data is removed beofre importing data to prevent duplicates 
+
+TRUNCATE TABLE bronze.crm_cust_info;
+LOAD DATA LOCAL INFILE '/Users/emilyramirez/Documents/sql-data-warehouse-project/datasets/source_crm/cust_info.csv'
+INTO TABLE bronze.crm_cust_info
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+TRUNCATE TABLE bronze.crm_prd_info;
+LOAD DATA LOCAL INFILE '/Users/emilyramirez/Documents/sql-data-warehouse-project/datasets/source_crm/prd_info.csv'
+INTO TABLE bronze.crm_prd_info
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+TRUNCATE TABLE bronze.crm_sales_details;
+LOAD DATA LOCAL INFILE '/Users/emilyramirez/Documents/sql-data-warehouse-project/datasets/source_crm/sales_details.csv'
+INTO TABLE bronze.crm_sales_details
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+TRUNCATE TABLE bronze.erp_cust_az12;
+LOAD DATA LOCAL INFILE '/Users/emilyramirez/Documents/sql-data-warehouse-project/datasets/source_erp/CUST_AZ12.csv'
+INTO TABLE bronze.erp_cust_az12
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+TRUNCATE TABLE bronze.erp_loc_a101;
+LOAD DATA LOCAL INFILE '/Users/emilyramirez/Documents/sql-data-warehouse-project/datasets/source_erp/LOC_A101.csv'
+INTO TABLE bronze.erp_loc_a101
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+LOAD DATA LOCAL INFILE '/Users/emilyramirez/Documents/sql-data-warehouse-project/datasets/source_erp/PX_CAT_G1V2.csv'
+INTO TABLE bronze.erp_px_cat_g1v2
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS;
+
+
+
+
+/*CHECKS TO MAKE SURE DATA WAS PROPERLY IMPORTED 
+SELECT * FROM bronze.crm_cust_info;
+SELECT * FROM bronze.crm_prd_info;
+SELECT * FROM bronze.crm_sales_details;
+SELECT * FROM bronze.erp_cust_az12;
+SELECT * FROM bronze.erp_loc_a101;
+SELECT * FROM bronze.erp_px_cat_g1v2;
+*/
